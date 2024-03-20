97a3aef23e11a1a16a0625ae6da8dbe57bfefc5f
FoundTexas <rolega01@gmail.com>
2024-03-20 00:21:15 -0600

    3/20/2024

--- assets/js/audioPlayer.js
+++ assets/js/audioPlayer.js
@@ -1,7 +1,7 @@
 // audioPlayer.js
 
 // Define the playSound function that takes an element ID as a parameter
-function playSound(elementId) {
+function playSound(elementId) { 
     console.log("audio");
     const audio = document.getElementById(elementId);
 
@@ -11,7 +11,6 @@ function playSound(elementId) {
         audio.play();
     } else {
         // Pause and reset the audio to the beginning
-
         audio.currentTime = 0;
     }
 }
@@ -19,9 +18,28 @@ function playSound(elementId) {
 // Function to set the volume of all audio elements
 function setVolume(value, group) {
     const audioElements = document.getElementsByClassName(group);
-    const volume = Math.pow((value), 1.1);
+    const volume = Math.pow(value, 1.1);
 
+    // Update volume percentage display
+    document.getElementById('volumeValue').textContent = `${Math.round(volume * 100)}%`;
+
+    // Set volume for each audio element
     for (let i = 0; i < audioElements.length; i++) {
         audioElements[i].volume = volume;
     }
+
+    // Store volume value as a cookie
+    document.cookie = `volume=${volume}; expires=Thu, 18 Dec 2025 12:00:00 UTC; path=/`;
 }
+
+
+// Retrieve volume value from cookie at document ready
+document.addEventListener('DOMContentLoaded', function() {
+    const volumeCookie = document.cookie.replace(/(?:(?:^|.*;\s*)volume\s*=\s*([^;]*).*$)|^.*$/, "$1");
+    if (volumeCookie) {
+        // Set volume slider value
+        document.getElementById('volumeRange').value = parseFloat(volumeCookie) ** (1/1.1);
+        // Set volume of audio elements
+        setVolume(parseFloat(volumeCookie), 'audio-group');
+    }
+});
