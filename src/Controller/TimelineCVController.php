9bac9e224e8fbc4327cd48ea4a3dee5f006382e5
FoundTexas <rolega01@gmail.com>
2024-04-04 01:14:19 -0600

    done timeline base

--- src/Controller/TimelineCVController.php
+++ src/Controller/TimelineCVController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\MileStone;
 use App\Entity\BulletPoints;
+use App\Entity\Project;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -17,6 +18,12 @@ class TimelineCVController extends AbstractController
         $mileStones = $entityManager->getRepository(MileStone::class)->findBy([], ['startDate' => 'ASC']);
         $bulletPoints = $entityManager->getRepository(BulletPoints::class)->findAll();
 
+        $projects = $entityManager->getRepository(Project::class)
+        ->createQueryBuilder('p')
+        ->innerJoin('p.mileStone', 'm', 'WITH', 'm IS NOT NULL')
+        ->getQuery()
+        ->getResult();
+
         $experiences = [];
 
         foreach ($mileStones as $milestone) {
@@ -24,6 +31,9 @@ class TimelineCVController extends AbstractController
         }
         foreach ($bulletPoints as $bulletPoint) {
             $experiences[$bulletPoint->getMilestone()->getId()]['bullets'][] = $bulletPoint;
+        }
+        foreach ($projects as $project) {
+            $experiences[$project->getMileStone()->getId()]['projects'][] = $project;
         }
 
         return $this->render('timeline_cv/index.html.twig', [
