7f05368ed8062a7e5f75479b718f00bef8a58d06
FoundTexas <rolega01@gmail.com>
2024-07-04 00:18:59 -0600

    tardis model

--- assets/js/webgl/webglTimeline.js
+++ assets/js/webgl/webglTimeline.js
@@ -2,10 +2,11 @@ import * as THREE from 'three';
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 
 let scene, camera, renderer;
-let geometries = []; // This will hold the loaded 3D model
-let raycaster = new THREE.Raycaster();
+let geometries = [];
 let mouse = new THREE.Vector2();
 let scrollY = 0;
+
+let scrollDir = 1;
 
 init();
 animate();
@@ -13,7 +14,8 @@ animate();
 function init() {
     scene = new THREE.Scene();
     camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-    camera.position.set(-50, 60, 80); // Adjust camera position
+    camera.position.set(-50, 66, 111); // Adjust camera position
+    camera.rotation.x=-0.2;
 
     renderer = new THREE.WebGLRenderer({
         canvas: document.querySelector('#bgcanvas'),
@@ -23,7 +25,7 @@ function init() {
     renderer.setPixelRatio(window.devicePixelRatio);
 
     // Add ambient light
-    const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); // Soft white light
+    const ambientLight = new THREE.AmbientLight(0xffffff, 0.1); // Soft white light
     scene.add(ambientLight);
 
     // Add directional light
@@ -67,7 +69,10 @@ function onMouseMove(event) {
 }
 
 function onScroll() {
+    const curscrollY = scrollY;
     scrollY = window.scrollY;
+
+    scrollDir = (scrollY > curscrollY)? 10 : -10;
 }
 
 function animate() {
@@ -80,15 +85,14 @@ function animate() {
 
     // Rotate model based on scroll direction
     geometries.forEach((model) => {
-        model.rotation.y = scrollY * 0.01; // Adjust rotation speed
+        model.rotation.y = ((scrollDir/100 ) + model.rotation.y); // Adjust rotation speed
     });
+
+    if(Math.abs(scrollDir) > 1)
+    {scrollDir += scrollDir > 1  ? -0.2 : 0.2;}
 
     // Camera sinusoidal motion
     camera.position.x = Math.sin(Date.now() * 0.00066) * 80; // Adjust speed and amplitude as needed
-
-    // Mouse hover interaction
-    raycaster.setFromCamera(mouse, camera);
-    // Note: Adjust raycaster logic based on your interaction needs
 
     renderer.render(scene, camera);
 }
